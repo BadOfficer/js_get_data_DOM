79a8b742fa1d2206dd47b0f0acf730e6da7117c8
BadOfficer <tarasbondarenko2005@gmail.com>
2025-08-17 22:50:44 +0300

    fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,37 @@
 'use strict';
 
 const populationsEl = document.querySelectorAll('.population');
-const totalPopulationEl = document.querySelector('.total');
-const averagePopulationEl = document.querySelector('.average');
+const totalPopulationEl = document.querySelector('.total-population');
+const averagePopulationEl = document.querySelector('.average-population');
 
 let total = 0;
+let count = 0;
 
 populationsEl.forEach((el) => {
   const num = Number.parseInt(el.textContent.replaceAll(',', ''));
 
-  total += num;
+  if (!isNaN(num)) {
+    total += num;
+    count++;
+  }
 });
 
-totalPopulationEl.textContent = total.toLocaleString();
+totalPopulationEl.textContent = formatNum(total);
 
-averagePopulationEl.textContent = Math.round(
-  total / populationsEl.length,
-).toLocaleString();
+averagePopulationEl.textContent =
+  count !== 0 ? formatNum(Math.round(total / count)) : 0;
+
+function formatNum(num, separator = ',') {
+  const numToStr = String(num);
+  let result = '';
+
+  for (let i = 0; i < numToStr.length; i++) {
+    if (i !== 0 && i % 3 === 0) {
+      result = separator + result;
+    }
+
+    result = numToStr[numToStr.length - i - 1] + result;
+  }
+
+  return result;
+}
